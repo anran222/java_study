8908d7493af348261171df9be980642c2bcf9f7b
anran222 <1432085874@qq.com>
2020-04-15 23:17:38 +0800

    创建树

---
+++ Map_interview/src/binarysearchtree/Node.java
@@ -0,0 +1,15 @@
+package binarysearchtree;
+
+/**
+ * @Author:xiang
+ * @Date:2020/4/15 23:16
+ */
+public class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        this.key = key;
+    }
+}
