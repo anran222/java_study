6c730b2927195d728978cbc15485bfede09008b0
anran222 <1432085874@qq.com>
2020-04-16 23:06:37 +0800

    删除链表中给定值val的所有节点

---
+++ LinkedList_interview/src/interview/Remove.java
@@ -0,0 +1,38 @@
+package interview;
+
+/**
+ * @Author:xiang
+ * @Date:2020/4/8 20:23
+ * 删除链表中给定值val的所有节点
+ */
+
+class ListNode{
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
+public class Remove {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head==null){
+            return null;
+        }
+        ListNode prev=head;
+        ListNode cur=head.next;
+        while (cur!=null){
+            if (cur.val==val){
+                prev.next=cur.next;
+                cur=cur.next;
+            }else{
+                prev=cur;
+                cur=cur.next;
+            }
+        }
+        if (head.val==val){
+            head=head.next;
+        }
+        return head;
+    }
+}
