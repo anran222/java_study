b04e80995db8a84aec28ec3e4b9e9a4b002d395c
anran222 <1432085874@qq.com>
2020-04-24 22:40:31 +0800

    业务逻辑层

--- student/src/org/student/service/StudentService.java
+++ student/src/org/student/service/StudentService.java
@@ -3,6 +3,8 @@ package org.student.service;
 import org.student.dao.StudentDao;
 import org.student.entity.Student;
 
+import java.util.List;
+
 /**
  * @Author:xiang
  * @Date:2020/4/23 22:53
@@ -11,13 +13,42 @@ import org.student.entity.Student;
 public class StudentService {
     StudentDao studentDao=new StudentDao();
 
+    //添加学生
     public boolean addStudent(Student student){
-        if (!studentDao.isExist(student.getSno())){
+        if (studentDao.isExist(student.getSno())==false){
             studentDao.addStudent(student);
             return true;
         }else {
             System.out.println("此人已存在！");
             return false;
         }
+    }
+
+    //删除学生
+    public boolean deleteStudentBySno(int sno){
+        if (studentDao.isExist(sno)) {
+           return studentDao.deleteStudent(sno);
+        }else {
+            return false;
+        }
+    }
+
+    //修改学生信息
+    public boolean updateStudentBySno(int sno,Student student){
+        if (studentDao.isExist(sno)){
+            return studentDao.updateStudentBySno(sno,student);
+        }else {
+            return false;
+        }
+    }
+
+    //查询学生
+    public Student queryStudentBySno(int sno){
+        return studentDao.queryStudentSno(sno);
+    }
+
+    //查询全部学生
+    public List<Student> queryAllStudents(){
+        return studentDao.queryAllStudents();
     }
 }
