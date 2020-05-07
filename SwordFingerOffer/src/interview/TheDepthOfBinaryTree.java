b005953411bc2b64ce6b9447c8212cdfba458e03
anran222 <1432085874@qq.com>
2020-05-07 23:48:49 +0800

    二叉树的深度

--- SwordFingerOffer/src/interview/TheDepthOfBinaryTree.java
+++ SwordFingerOffer/src/interview/TheDepthOfBinaryTree.java
@@ -15,8 +15,11 @@ public class TheDepthOfBinaryTree {
             this.val = val;
         }
     }
-//
-//    public static int maxDepth(TreeNode root) {
-//
-//    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root==null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
 }
