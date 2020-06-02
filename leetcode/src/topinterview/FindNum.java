17b35c061b19d46344c54b5592c5d880e7399b18
anran222 <1432085874@qq.com>
2020-06-02 23:53:52 +0800

    找到不在序列中的那个数

---
+++ leetcode/src/topinterview/FindNum.java
@@ -0,0 +1,32 @@
+package topinterview;
+
+/**
+ * @Author:xiang
+ * @Date:2020/6/2 23:32
+ * 找出不在序列中的那个数
+ */
+public class FindNum {
+    public static void main(String[] args) {
+        int[] nums={3,0,1};
+        System.out.println(missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int max=nums[0];
+        for (int i = 1; i <nums.length; i++) {
+            if (nums[i]>max){
+                max=nums[i];
+            }
+        }
+        int sum1=0;
+        int sum2=0;
+        for (int i = 0; i <=max ; i++) {
+            sum1+=i;
+        }
+        for (int i = 0; i <nums.length ; i++) {
+            sum2+=nums[i];
+        }
+        int num=sum1-sum2;
+        return num;
+    }
+}
