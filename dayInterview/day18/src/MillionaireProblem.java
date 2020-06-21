a32dee1d187dcd86354b519abb051b34f70d9fdd
anran222 <1432085874@qq.com>
2020-06-21 23:13:58 +0800

    百万富翁问题

---
+++ dayInterview/day18/src/MillionaireProblem.java
@@ -0,0 +1,14 @@
+/**
+ * @Author:xiang
+ * @Date:2020/6/21 22:36
+ * 百万富翁问题
+ */
+public class MillionaireProblem {
+    public static void main(String[] args) {
+        int num1=0;
+        int num2=0;
+        num1=10*30;
+        num2=(int)(Math.pow(2,30)-1);
+        System.out.println(num1+" "+num2);
+    }
+}
