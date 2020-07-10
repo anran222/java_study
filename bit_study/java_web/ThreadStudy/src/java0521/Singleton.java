15204c796eb0e6220f7da7e06b8c0e0b32cca25b
anran222 <1432085874@qq.com>
2020-07-10 21:45:49 +0800

    单例模式

---
+++ bit_study/java_web/ThreadStudy/src/java0521/Singleton.java
@@ -0,0 +1,30 @@
+package java0521;
+
+/**
+ * @Author:xiang
+ * @Date:2020/7/10 21:30
+ * 单例模式：在某些场景下，需要获取同一个对象
+ */
+public class Singleton {
+
+    //饿汉式：在类加载的时候，就创建对象
+    public static Singleton SINGLETON=new Singleton();
+
+    private Singleton(){
+    }
+
+    public static Singleton getInstance(){
+        return SINGLETON;
+    }
+
+    //懒汉式：
+    public static Singleton SING_LETON2;
+
+    //省略了构造方法
+    public static Singleton getInstance2(){
+        if (SING_LETON2==null) {
+            SING_LETON2 = new Singleton();
+        }
+            return SING_LETON2;
+    }
+}
