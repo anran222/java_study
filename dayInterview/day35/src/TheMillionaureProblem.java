efedac9c6006438036eddab8f4570cf61d0b8703
anran222 <1432085874@qq.com>
2020-07-12 19:06:56 +0800

    百万富翁问题

---
+++ dayInterview/day35/src/TheMillionaureProblem.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+
+/**
+ * @Author:xiang
+ * @Date:2020/7/12 18:11
+ * 百万富翁问题
+ */
+public class TheMillionaureProblem {
+    public static void main(String[] args) {
+        int n=10*30;
+        int m=0;
+        m=(int)(Math.pow(2,30)-1);
+        System.out.print(n+" "+m);
+    }
+}
