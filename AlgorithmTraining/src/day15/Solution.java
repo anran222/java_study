8b05e630d43ff09c9828a2e2aefa5aae2a20f226
anran222 <1432085874@qq.com>
2020-08-07 22:38:02 +0800

    排序

---
+++ AlgorithmTraining/src/day15/Solution.java
@@ -0,0 +1,169 @@
+package day15;
+
+import java.util.Arrays;
+
+/**
+ * @Author:xiang
+ * 排序
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] array={9,5,2,7,3,6,8};
+        insertSort(array);
+        System.out.println(Arrays.toString(array));
+        int[] array1={9,5,2,7,3,6,8};
+        shellSort(array1);
+        System.out.println(Arrays.toString(array1));
+        int[] array2={9,5,2,7,3,6,8};
+        selectSort(array2);
+        System.out.println(Arrays.toString(array2));
+        int[] array3={9,5,2,7,3,6,8};
+        bubbleSort(array3);
+        System.out.println(Arrays.toString(array3));
+        int[] array4={9,5,2,7,3,6,8};
+        quickSort(array4);
+        System.out.println(Arrays.toString(array4));
+        int[] array5={9,5,2,7,3,6,8};
+        mergeSort(array5);
+        System.out.println(Arrays.toString(array5));
+    }
+
+    public  static void insertSort(int[] array){
+        for (int i = 1; i <array.length ; i++) {
+            int v=array[i];
+            int cur=i-1;
+            for (; cur>=0 ; cur--) {
+                if (array[cur]>v){
+                    array[cur+1]=array[cur];
+                }else {
+                    break;
+                }
+            }
+            array[cur+1]=v;
+        }
+    }
+
+    public static void shellSort(int[] array){
+        int gap=array.length/2;
+        while (gap>1){
+            shellSortHelper(array,gap);
+            gap=gap/2;
+        }
+        shellSortHelper(array,gap);
+    }
+    public static void shellSortHelper(int[] array,int gap){
+        for (int i = gap; i <array.length ; i++) {
+            int v=array[i];
+            int cur=i-gap;
+            for (; cur >=0 ;cur-=gap ) {
+                if (array[cur]>v){
+                    array[cur+gap]=array[cur];
+                }else {
+                    break;
+                }
+            }
+            array[cur+gap]=v;
+        }
+    }
+
+    public static void selectSort(int[] array){
+        for (int i = 0; i <array.length ; i++) {
+            for (int j = i+1; j <array.length ; j++) {
+                if (array[i]>array[j]){
+                    int temp=array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+    }
+
+    public static void bubbleSort(int[] array){
+        for (int i = 0; i <array.length ; i++) {
+            for (int j = array.length-1; j>i ; j--) {
+                if (array[j-1]>array[j]){
+                    int temp=array[j-1];
+                    array[j-1]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+    }
+
+    public static void quickSort(int[] array){
+        quickSortHelper(array,0,array.length-1);
+    }
+    public static void quickSortHelper(int[] array,int left,int right){
+        if (left>=right){
+            return;
+        }
+        int index=partition(array,left,right);
+        quickSortHelper(array,left,index-1);
+        quickSortHelper(array,index+1,right);
+    }
+    public static int partition(int[] array,int left,int right){
+        int i=left;
+        int j=right;
+        int base=array[right];
+        while (i<j){
+            while (i<j&&array[i]<=base){
+                i++;
+            }
+            while (i<j&&array[j]>=base){
+                j--;
+            }
+            swap(array,i,j);
+        }
+        swap(array,i,right);
+        return i;
+    }
+    public static void swap(int[] array,int i,int j){
+        int temp=array[i];
+        array[i]=array[j];
+        array[j]=temp;
+    }
+
+    public static void mergeSort(int[] array){
+        mergeSortHelper(array,0,array.length);
+    }
+    public static void mergeSortHelper(int[] array,int low,int high){
+        if ((high-low)<=1){
+            return;
+        }
+        int mid=(low+high)/2;
+        mergeSortHelper(array,low,mid);
+        mergeSortHelper(array,mid,high);
+        merge(array,low,mid,high);
+    }
+    public static void merge(int[] array,int low,int mid,int high){
+        int i=low;
+        int j=mid;
+        int[] index=new int[high+low];
+        int k=0;
+        while (i<mid&&j<high){
+            if (array[i]<array[j]){
+                index[k]=array[i];
+                k++;
+                i++;
+            }else {
+                index[k]=array[j];
+                k++;
+                j++;
+            }
+        }
+        while (i<mid){
+            index[k]=array[i];
+            k++;
+            i++;
+        }
+        while (j<high){
+            index[k]=array[j];
+            k++;
+            j++;
+        }
+        for (int l = 0; l <(high-low) ; l++) {
+            array[low+l]=index[l];
+        }
+    }
+
+}
