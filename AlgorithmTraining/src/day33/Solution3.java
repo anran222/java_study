0c0f502a3dddc0fcd5dcad9bfb20a68dc57488b5
anran222 <1432085874@qq.com>
2020-09-14 22:04:19 +0800

    求最小的k个数

---
+++ AlgorithmTraining/src/day33/Solution3.java
@@ -0,0 +1,42 @@
+package day33;
+/*
+ *@Author:anran
+ *@Date:2020/9/14
+ *@Version 1.0
+ * topK 求最小的k个数
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        int[] input={4,5,1,6,2,7,3,8};
+        int k=4;
+        for (Integer l:GetLeastNumbers_Solution(input,k)) {
+            System.out.println(l);
+        }
+    }
+    public static ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> list=new ArrayList<>();
+        if (input.length<0||k>input.length||k<=0){
+            return list;
+        }
+        PriorityQueue<Integer> queue=new PriorityQueue<>(k, Collections.reverseOrder());
+        for (int i = 0; i <input.length ; i++) {
+            if (i<k){
+                queue.offer(input[i]);
+            }else {
+                if(input[i]<queue.peek()){
+                    queue.poll();
+                    queue.offer(input[i]);
+                }
+            }
+        }
+        for (int i = 0; i <k ; i++) {
+            list.add(queue.poll());
+        }
+        return list;
+    }
+}
