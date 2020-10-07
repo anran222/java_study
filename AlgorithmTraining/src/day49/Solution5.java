4001c29574fc78ce527921353872f2b3187345e8
anran222 <1432085874@qq.com>
2020-10-07 17:01:37 +0800

    最小深度平衡二叉树

---
+++ AlgorithmTraining/src/day49/Solution5.java
@@ -0,0 +1,32 @@
+package day49;
+/*
+ *@Author:anran
+ *@Date:2020/10/7
+ *@Version 1.0
+ * 最小深度树
+ */
+
+public class Solution5 {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val){
+            this.val=val;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums,0,nums.length-1);
+    }
+    public TreeNode helper(int[] nums,int left,int right){
+        if (left>right){
+            return null;
+        }
+        int mid=(left+right)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+        root.left=helper(nums,left,mid-1);
+        root.right=helper(nums,mid+1,right);
+        return root;
+    }
+}
